79947d3965bf019443a0beb720c8ea947e7077e4
Karl Macadangdang <karljarren0308@yahoo.com>
2018-05-23 04:21:07 +0800

    Renamed from Login.js

---
+++ styles/LoginStyles.js
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  page: {
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 25,
+    paddingRight: 25
+  }
+});
