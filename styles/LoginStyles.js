42fb3027676ef656e0d80cbf79ed3f4ab9b25f88
Karl Macadangdang <karljarren0308@yahoo.com>
2018-05-23 09:46:07 +0800

    New styles

--- styles/LoginStyles.js
+++ styles/LoginStyles.js
@@ -1,14 +1,63 @@
 import {StyleSheet} from 'react-native';
 
+import {Fonts} from './Fonts';
+
+const dangerColor = '#c73a4e';
+const infoColor = '#5c8dbf';
+const successColor = '#5abf79';
+const primaryColor = '#4c9261';
+const warningColor = '#eacf6c';
+
 export default StyleSheet.create({
-  page: {
-    alignItems: 'center',
-    backgroundColor: '#f5f5f5',
+  header: {
+    backgroundColor: primaryColor,
+    borderRadius: 450,
+    marginTop: -621,
+    marginLeft: -270,
+    height: 900,
+    width: 900
+  },
+  headerContent: {
+    marginTop: 648,
+    marginLeft: 300,
+    width: 300
+  },
+  headerLogo: {
+    left: '50%',
+    marginLeft: -75,
+    height: 150,
+    width: 150
+  },
+  headerTitleText: {
+    color: 'white',
+    fontFamily: 'Satisfy',
+    fontSize: 50,
+    textAlign: 'center',
+    textShadowColor: '#222222',
+    textShadowOffset: {
+      height: 2,
+      width: 0
+    },
+    textShadowRadius: 4
+  },
+  body: {
     flex: 1,
+    padding: 30
+  },
+  footer: {
+    backgroundColor: '#f5f5f5',
     justifyContent: 'center',
-    paddingTop: 15,
-    paddingBottom: 15,
-    paddingLeft: 25,
-    paddingRight: 25
+    padding: 15
+  },
+  footerText: {
+    fontSize: 10,
+    textAlign: 'center'
+  },
+  buttonLinkBlock: {
+    alignItems: 'center',
+    marginTop: 10
+  },
+  buttonLink: {
+    padding: 5
   }
 });
--- styles/Styles.js
+++ styles/Styles.js
@@ -3,12 +3,17 @@ import {StyleSheet} from 'react-native';
 const dangerColor = '#c73a4e';
 const infoColor = '#5c8dbf';
 const successColor = '#5abf79';
+const primaryColor = '#4c9261';
 const warningColor = '#eacf6c';
 
 export default StyleSheet.create({
   body: {
+    backgroundColor: 'white',
     height: '100%',
     width: '100%'
+  },
+  colorWhite: {
+    color: 'white'
   },
   container: {
     paddingTop: 15,
@@ -16,17 +21,24 @@ export default StyleSheet.create({
     paddingLeft: 25,
     paddingRight: 25
   },
+  buttonPrimary: {
+    alignItems: 'center',
+    backgroundColor: primaryColor,
+    padding: 15
+  },
+  buttonContent: {
+    color: 'white',
+    fontWeight: 'bold'
+  },
   inputControl: {
-    height: 40,
+    marginBottom: 5,
+    height: 50,
     width: '100%'
   },
   modal: {
     justifyContent: 'center',
     backgroundColor: 'rgba(34, 34, 34, 0.5)',
     flex: 1,
-    padding: 15
-  },
-  notifModal: {
     padding: 15
   },
   modalContent: {
@@ -36,18 +48,30 @@ export default StyleSheet.create({
   },
   promptBoxDanger: {
     backgroundColor: dangerColor,
-    color: 'white'
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 25,
+    paddingRight: 25
   },
   promptBoxInfo: {
     backgroundColor: infoColor,
-    color: 'white'
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 25,
+    paddingRight: 25
   },
   promptBoxSuccess: {
     backgroundColor: successColor,
-    color: 'white'
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 25,
+    paddingRight: 25
   },
   promptBoxWarning: {
     backgroundColor: warningColor,
-    color: 'white'
+    paddingTop: 15,
+    paddingBottom: 15,
+    paddingLeft: 25,
+    paddingRight: 25
   }
 });
